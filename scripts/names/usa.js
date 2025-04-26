03ed6e8f53e008df24e2c338289d7ad9a129f789
Masahisa Miyauchi <miyamasa@gmail.com>
2025-04-26 11:59:07 +0900

    アメリカの名前データファイルを作成

---
+++ scripts/names/usa.js
@@ -0,0 +1,63 @@
+/**
+ * アメリカの名前データ
+ * アプリケーションで使用するアメリカ人の名前のサンプルデータセット
+ */
+
+const AmericanNames = [
+    // 男性名
+    { id: 1, firstName: 'James', lastName: 'Smith', gender: 'male' },
+    { id: 2, firstName: 'Robert', lastName: 'Johnson', gender: 'male' },
+    { id: 3, firstName: 'John', lastName: 'Williams', gender: 'male' },
+    { id: 4, firstName: 'Michael', lastName: 'Brown', gender: 'male' },
+    { id: 5, firstName: 'William', lastName: 'Jones', gender: 'male' },
+    { id: 6, firstName: 'David', lastName: 'Garcia', gender: 'male' },
+    { id: 7, firstName: 'Richard', lastName: 'Miller', gender: 'male' },
+    { id: 8, firstName: 'Joseph', lastName: 'Davis', gender: 'male' },
+    { id: 9, firstName: 'Thomas', lastName: 'Rodriguez', gender: 'male' },
+    { id: 10, firstName: 'Charles', lastName: 'Martinez', gender: 'male' },
+    { id: 11, firstName: 'Christopher', lastName: 'Hernandez', gender: 'male' },
+    { id: 12, firstName: 'Daniel', lastName: 'Lopez', gender: 'male' },
+    { id: 13, firstName: 'Matthew', lastName: 'Gonzalez', gender: 'male' },
+    { id: 14, firstName: 'Anthony', lastName: 'Wilson', gender: 'male' },
+    { id: 15, firstName: 'Mark', lastName: 'Anderson', gender: 'male' },
+    { id: 16, firstName: 'Donald', lastName: 'Taylor', gender: 'male' },
+    { id: 17, firstName: 'Steven', lastName: 'Thomas', gender: 'male' },
+    { id: 18, firstName: 'Paul', lastName: 'Jackson', gender: 'male' },
+    { id: 19, firstName: 'Andrew', lastName: 'White', gender: 'male' },
+    { id: 20, firstName: 'Joshua', lastName: 'Harris', gender: 'male' },
+    { id: 21, firstName: 'Kenneth', lastName: 'Martin', gender: 'male' },
+    { id: 22, firstName: 'Kevin', lastName: 'Thompson', gender: 'male' },
+    { id: 23, firstName: 'Brian', lastName: 'Moore', gender: 'male' },
+    { id: 24, firstName: 'George', lastName: 'Allen', gender: 'male' },
+    { id: 25, firstName: 'Timothy', lastName: 'Young', gender: 'male' },
+    
+    // 女性名
+    { id: 26, firstName: 'Mary', lastName: 'King', gender: 'female' },
+    { id: 27, firstName: 'Patricia', lastName: 'Wright', gender: 'female' },
+    { id: 28, firstName: 'Jennifer', lastName: 'Scott', gender: 'female' },
+    { id: 29, firstName: 'Linda', lastName: 'Green', gender: 'female' },
+    { id: 30, firstName: 'Elizabeth', lastName: 'Baker', gender: 'female' },
+    { id: 31, firstName: 'Barbara', lastName: 'Adams', gender: 'female' },
+    { id: 32, firstName: 'Susan', lastName: 'Nelson', gender: 'female' },
+    { id: 33, firstName: 'Jessica', lastName: 'Hill', gender: 'female' },
+    { id: 34, firstName: 'Sarah', lastName: 'Ramirez', gender: 'female' },
+    { id: 35, firstName: 'Karen', lastName: 'Campbell', gender: 'female' },
+    { id: 36, firstName: 'Lisa', lastName: 'Mitchell', gender: 'female' },
+    { id: 37, firstName: 'Nancy', lastName: 'Roberts', gender: 'female' },
+    { id: 38, firstName: 'Betty', lastName: 'Carter', gender: 'female' },
+    { id: 39, firstName: 'Margaret', lastName: 'Phillips', gender: 'female' },
+    { id: 40, firstName: 'Sandra', lastName: 'Evans', gender: 'female' },
+    { id: 41, firstName: 'Ashley', lastName: 'Turner', gender: 'female' },
+    { id: 42, firstName: 'Kimberly', lastName: 'Torres', gender: 'female' },
+    { id: 43, firstName: 'Emily', lastName: 'Parker', gender: 'female' },
+    { id: 44, firstName: 'Donna', lastName: 'Collins', gender: 'female' },
+    { id: 45, firstName: 'Michelle', lastName: 'Edwards', gender: 'female' },
+    { id: 46, firstName: 'Carol', lastName: 'Stewart', gender: 'female' },
+    { id: 47, firstName: 'Amanda', lastName: 'Flores', gender: 'female' },
+    { id: 48, firstName: 'Melissa', lastName: 'Morris', gender: 'female' },
+    { id: 49, firstName: 'Deborah', lastName: 'Nguyen', gender: 'female' },
+    { id: 50, firstName: 'Stephanie', lastName: 'Murphy', gender: 'female' }
+];
+
+// グローバルオブジェクトとしてエクスポート
+window.AmericanNames = AmericanNames;
