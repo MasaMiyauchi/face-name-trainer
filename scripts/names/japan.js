e6a50c4f8f6a868044bf1806655fe19af82a7cc7
Masahisa Miyauchi <miyamasa@gmail.com>
2025-04-26 11:58:08 +0900

    日本の名前データファイル（前半）を作成

---
+++ scripts/names/japan.js
@@ -0,0 +1,32 @@
+/**
+ * 日本の名前データ
+ * アプリケーションで使用する日本人の名前のサンプルデータセット
+ */
+
+const JapaneseNames = [
+    // 男性名
+    { id: 1, firstName: '大翔', lastName: '佐藤', gender: 'male' },
+    { id: 2, firstName: '翔太', lastName: '鈴木', gender: 'male' },
+    { id: 3, firstName: '悠真', lastName: '高橋', gender: 'male' },
+    { id: 4, firstName: '陽斗', lastName: '田中', gender: 'male' },
+    { id: 5, firstName: '蓮', lastName: '伊藤', gender: 'male' },
+    { id: 6, firstName: '湊', lastName: '渡辺', gender: 'male' },
+    { id: 7, firstName: '颯真', lastName: '山本', gender: 'male' },
+    { id: 8, firstName: '樹', lastName: '中村', gender: 'male' },
+    { id: 9, firstName: '悠太', lastName: '小林', gender: 'male' },
+    { id: 10, firstName: '悠', lastName: '加藤', gender: 'male' },
+    { id: 11, firstName: '大和', lastName: '吉田', gender: 'male' },
+    { id: 12, firstName: '達也', lastName: '山田', gender: 'male' },
+    { id: 13, firstName: '健太', lastName: '佐々木', gender: 'male' },
+    { id: 14, firstName: '一郎', lastName: '山口', gender: 'male' },
+    { id: 15, firstName: '太郎', lastName: '松本', gender: 'male' },
+    { id: 16, firstName: '直樹', lastName: '井上', gender: 'male' },
+    { id: 17, firstName: '大輔', lastName: '木村', gender: 'male' },
+    { id: 18, firstName: '和也', lastName: '林', gender: 'male' },
+    { id: 19, firstName: '哲也', lastName: '斎藤', gender: 'male' },
+    { id: 20, firstName: '拓海', lastName: '清水', gender: 'male' },
+    { id: 21, firstName: '翔', lastName: '山崎', gender: 'male' },
+    { id: 22, firstName: '隆', lastName: '森', gender: 'male' },
+    { id: 23, firstName: '淳', lastName: '池田', gender: 'male' },
+    { id: 24, firstName: '誠', lastName: '橋本', gender: 'male' },
+    { id: 25, firstName: '優太', lastName: '阿部', gender: 'male' },
